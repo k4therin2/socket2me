962aee853f096d76cbc982c7e5014ec1492951bc
Katherine Cass <cass@case.edu>
2015-10-09 12:26:57 -0400

    i have some idea what is going on

--- rclient.rb
+++ rclient.rb
@@ -1,13 +1,11 @@
 require 'socket'
 
 hostname = 'ec2-52-4-246-92.compute-1.amazonaws.com'
-port = 2000
+port = 5251 
 
 sock = TCPSocket.open(hostname, port)
-puts "about to enter loop"
 while line = sock.gets
-  puts 'in loop'
-  outs line.chop
+  puts line.chop
 end
 
 sock.close
