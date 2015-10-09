87097a4f1c628eeba07dcfb02268c85394b476e4
Ubuntu <ubuntu@ip-172-31-58-54.ec2.internal>
2015-10-09 15:47:06 +0000

    added message to server

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require 'socket'
 server = TCPServer.open(2000)
 loop{
   client = server.accept #wait for a client to connect
+  puts 'Someone is here!'
   client.puts("I was sleeping, this better be important") #send the time to client
   client.puts "Closing the connection. Later losers" #say goodbye
   client.close  #disconnect from client
