79b3bae7469710779d0ee967ed7d0aab1e0d1df9
Ubuntu <ubuntu@ip-172-31-58-54.ec2.internal>
2015-10-09 15:34:59 +0000

    added sass

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require 'socket'
 server = TCPServer.open(2000)
 loop{
   client = server.accept #wait for a client to connect
-  client.puts(Time.now.ctime) #send the time to client
+  client.puts("I was sleeping, this better be important") #send the time to client
   client.puts "Closing the connection. Later losers" #say goodbye
   client.close  #disconnect from client
 }
